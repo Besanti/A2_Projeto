9204deb93ab9a7da1cfecf4771c035a720aef2e7
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-06-20 08:29:38 -0300

    Update SITE_TESTE.py

--- SITE_TESTE.py
+++ SITE_TESTE.py
@@ -6,5 +6,5 @@ import streamlit as st
 
 st.title("Localizador de Jogadores")
 
-title = st.text_input('Digite o nome de um jogador do Real Madrid', '.')
+title = st.text_input('Digite o nome de um jogador do Real Madrid', ':')
 st.write('Informações sobre o Jogador', title)
